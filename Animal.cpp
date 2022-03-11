b06936b584ce984306e3db24cef7f988a92677c1
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2022-03-10 14:49:12 -1000

    Initial commit

---
+++ Animal.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
+///
+/// @file Animal.cpp
+/// @version 1.0
+///
+/// @author  Aksel Sloan <@aksel@hawaii.edu>
+/// @date   10_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Animal.h"
+
+Animal::Animal() {}
---
+++ Animal.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
+///
+/// @file Animal.h
+/// @version 1.0
+///
+/// @author  Aksel Sloan <@aksel@hawaii.edu>
+/// @date   10_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_LAB08C_ALL_CREATURES_GREAT_AND_SMALL_ANIMAL_H
+#define EE205_LAB08C_ALL_CREATURES_GREAT_AND_SMALL_ANIMAL_H
+
+
+class Animal {
+public:
+    Animal();
+};
+
+
+#endif //EE205_LAB08C_ALL_CREATURES_GREAT_AND_SMALL_ANIMAL_H
